0f281fe950fd8cf63473a598c7fba79673f44108
hr4official <hardikrthod3695@gmail.com>
2019-11-20 10:48:43 +0530

    pdf to txt

--- pdf-to-text.py
+++ pdf-to-text.py
@@ -5,6 +5,6 @@ x=pdfreader.numPages
 pageobj=pdfreader.getPage(x-1)
 text=pageobj.extractText()
 
-file1=open(r"(folder path)\\1.txt","a")
+file1=open(r"created_text.txt","a")
 file1.writelines(text)
 file1.close()
